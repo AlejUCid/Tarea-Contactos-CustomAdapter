c8c97f8ba11b89792e6be2bc2e742e798fd859a0
AlejandroCid <alejandrociduroza@gmail.com>
2024-10-13 21:10:03 -0600

    ContactoModelo.kt
    La estructura de los datos del contacto

---
+++ ContactoModelo.kt
@@ -0,0 +1,6 @@
+package com.example.listacontactosconcustonadapter
+
+class ContactoModel (   val nombreCont: String,
+                        val telefCont: String,
+                        val emailCont: String,
+                        val imagCont: Int)
